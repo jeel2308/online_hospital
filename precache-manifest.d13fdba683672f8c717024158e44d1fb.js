a2e87a8cc81a8116fbc8c9ac2d62bd9f74faf5b5
jeel2308 <jeelpatel238@gmail.com>
2020-06-16 15:36:50 +0530

    Updates

---
+++ precache-manifest.d13fdba683672f8c717024158e44d1fb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfde228b5d981a85fa806a4129276be1",
+    "url": "/online_hospital/index.html"
+  },
+  {
+    "revision": "6198c8d2bdb8f8ccc688",
+    "url": "/online_hospital/static/css/main.677c2d71.chunk.css"
+  },
+  {
+    "revision": "5e3b2ca9a1fa484cb345",
+    "url": "/online_hospital/static/js/2.1dea38b5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/online_hospital/static/js/2.1dea38b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6198c8d2bdb8f8ccc688",
+    "url": "/online_hospital/static/js/main.ea9f0d6d.chunk.js"
+  },
+  {
+    "revision": "fc9efb14371b01946585",
+    "url": "/online_hospital/static/js/runtime-main.7161d7d9.js"
+  },
+  {
+    "revision": "9ab624c183a6e04a8b08da9d16fac4b3",
+    "url": "/online_hospital/static/media/509623.9ab624c1.jpg"
+  },
+  {
+    "revision": "bbcf4afc68915b1a75c459cd4db5aac1",
+    "url": "/online_hospital/static/media/Flash.bbcf4afc.jpg"
+  },
+  {
+    "revision": "8b7d8871dec075a5ba83b3e2d198d1bc",
+    "url": "/online_hospital/static/media/clones.8b7d8871.jpg"
+  }
+]);
